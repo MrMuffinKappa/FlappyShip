7b766dc048e73a6165eb3f2b1079dc392916935f
Chlebik Dávid <99025032+MrMuffinKappa@users.noreply.github.com>
2023-05-17 00:44:43 +0200

    Add rotation by flapping

---
+++ FlappyShip/Assets/Scripts/CameraRail.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRail : MonoBehaviour
+{
+
+    public Vector3 startPos;
+    public Vector3 endPos;
+    public Quaternion startRotation;
+    public Quaternion endRotation;
+    public float startSize;
+    public float endSize;
+    private Camera camera;
+    public float duration = 2.0f;
+
+    void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+    void Start()
+    {
+        StartCoroutine(MoveAndRotateCamera());
+    }
+
+    IEnumerator MoveAndRotateCamera()
+    {
+        float elapsed = 0;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, t);
+            camera.orthographicSize = Mathf.Lerp(startSize, endSize, t);
+
+            yield return null;
+        }
+
+        // Ensure it's fully moved to the end position.
+        transform.position = endPos;
+        transform.rotation = endRotation;
+    }
+}
+
--- FlappyShip/Assets/Scripts/FlappyMovement.cs
+++ FlappyShip/Assets/Scripts/FlappyMovement.cs
@@ -7,9 +7,17 @@ public class FlappyMovement : MonoBehaviour
     public float flapForce = 5f;
     public float gravityForce = 9.8f;
     public bool isEnable = true;
+    public float maxRotate = 40f;
+    public float rotateForce = 40f;
+    public AnimationCurve rollCurve;
+    public float _currentRoll, _targetRoll;
 
+
+    private float rotateVelocity;
     private float verticalVelocity;
     private Rigidbody rb;
+
+
 
 
 
@@ -25,20 +33,40 @@ public class FlappyMovement : MonoBehaviour
         if (Input.GetMouseButton(0) || Input.GetButtonDown("Jump"))
         {
             verticalVelocity = flapForce;
+            _currentRoll = 0;
+            _targetRoll = 1;
+
         }
 
         if (isEnable)
         {
-        verticalVelocity -= gravityForce * Time.deltaTime;
-        float newPosX = transform.position.x - verticalVelocity * Time.deltaTime;
-
-        rb.MovePosition(new Vector2(Mathf.Clamp(newPosX, -1 * gravityForce, gravityForce), transform.position.y));
+            MoveShip();
+            RotateShipNew();
         }
     }
 
     public IEnumerator StartGame()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
         isEnable = true;
     }
+
+    void MoveShip()
+    {
+        verticalVelocity -= gravityForce * Time.deltaTime;
+        float newPosX = transform.position.x - verticalVelocity * Time.deltaTime;
+        rb.MovePosition(new Vector2(Mathf.Clamp(newPosX, -1 * gravityForce, gravityForce), transform.position.y));
+    }
+
+
+    public void RotateShipNew()
+    {
+        //transform.eulerAngles = Vector3.Lerp(new Vector3(-90,0,0),new Vector3(-90,359,0), rollCurve.Evaluate())
+        _currentRoll = Mathf.MoveTowards(_currentRoll, _targetRoll, 0.9f * Time.deltaTime);
+        float _rotation = Mathf.Lerp(0, 360, rollCurve.Evaluate(_currentRoll));
+        transform.rotation = Quaternion.Euler(new Vector3(-90, _rotation, 0));
+        //transform.rotation = Quaternion.Lerp(Quaternion.Euler(new Vector3(-90, 0, 0)), Quaternion.Euler(new Vector3(-90, 360, 0)), rollCurve.Evaluate(_currentRoll));
+    }
+
+
 }
